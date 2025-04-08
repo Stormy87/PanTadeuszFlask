124e0fdaa20a947e601fe0a40af23349d4cfb7bf
Stormy87 <tomek.jarzabek87@gmail.com>
2025-04-08 10:16:28 +0200

    222

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/") 
-@app.route("/<name>") 
+@app.route() 
 def index(name = "World"):
-    return render_template('index.html', name = name)
+    return render_template('index.html')
+
+@app.route("/book/<id>")
+def book(id):
+    return render_template(f'{id}.html')
 
 if __name__ == "__main__": 
     app.run()
